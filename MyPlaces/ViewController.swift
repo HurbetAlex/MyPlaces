43421b09895c2f95f373f9eb98a1825b1afc199f
Alex Hurbet <alexhurbet@icloud.com>
2022-06-06 13:38:44 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Александр Матюшенко on 06.06.2022.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = "Cell"
+        return cell!
+    }
+
+}
+
